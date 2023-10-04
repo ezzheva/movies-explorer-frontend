598d0c26cc6bc0908613d9a97bfc78889a0c1b08
unknown <ezzheva.ira@yandex.ru>
2023-10-04 17:36:02 +0300

    button

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,22 +1,21 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 import "./NotFound.css";
 
 function NotFound() {
-  const navigate = useNavigate();
+  const history = useHistory();
 
-  function goBackPage(evt) {
-    evt.preventDefault();
-    navigate(-1);
+  function goBackPage() {
+    history.goBack();
   }
 
   return (
     <section className="not-found">
       <h1 className="not-found__title">404</h1>
       <p className="not-found__subtitle">Страница не найдена</p>
-      <Link to="" className="not-found__button" onClick={goBackPage}>
+      <button className="not-found__button" onClick={goBackPage}>
         Назад
-      </Link>
+      </button>
     </section>
   );
 }
